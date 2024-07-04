a43f13b8dc6e6604274284fdfa848272d67184c1
Guido Cardarelli <guidocarda@hotmail.com>
2024-07-04 10:37:11 -0300

    add basic countdown timer

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@ function $(selector, context = document) {
 function $$(selector, context = document) {
   return context.querySelectorAll(selector);
 }
+
+var $timer = $(".timer");
+var timer_options = [60, 120, 180];
+
+function timer(time = 10) {
+  var intervalRef;
+  var remainingTime = time;
+
+  if (!intervalRef) {
+    intervalRef = setInterval(function () {
+      $timer.textContent = remainingTime;
+
+      if (remainingTime <= 0) {
+        return clearInterval(intervalRef);
+      }
+
+      remainingTime = remainingTime - 1;
+    }, 1000);
+  }
+}
+
+timer();
